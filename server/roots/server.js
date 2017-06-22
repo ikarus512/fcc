8ac4ddc564d055ec57cc1eb2390c39e8fd94dfa3
ikarus512 <ikarus512@yandex.ru>
2017-06-23 00:05:36 +0300

    cosmetic

--- server/roots/server.js
+++ server/roots/server.js
@@ -5,42 +5,48 @@ var express = require('express'),
   request = require('request');
 
 router.get(/^\/w\/.+/, function (req, res) {
-  var searchstr = req.url.replace(/^\/w\//,'');
-//router.get('/w/', function (req, res) {
-
-  request.get(
-    'http://meteoinfo.ru/forecasts5000/russia/'+searchstr,
-    //'http://meteoinfo.ru/forecasts5000/russia/moscow-area/moscow',
-    //'http://meteoinfo.ru/forecasts5000/russia/nizhegorodskaya-area',
-    function(err, response, data) {
-      if(err || response.statusCode !== 200) {
-        res.json({ "error": err, statusCode: response.statusCode });
-      } else {
-        // data = JSON.parse(data);
-        res.json(data);
-      }
-    }
-  );
-
-  function extractVal(str,name) {
-    var re = new RegExp(name+'=(\\[\\{[^;]*);', 'i');
-    var s;
-    try {
-      s = eval(str.match(re)[1]);
-    } catch(err) {
-    }
-    return s;
-  }
-
-  var
-    // data = require('./1.js'),
+  var searchstr = req.url.replace(/^\/w\//,''),
     arr_temperature = extractVal(data, 'arr_temperature'),
     arr_precip_val = extractVal(data, 'arr_precip_val'),
-    arr_precip_ver = extractVal(data, 'arr_precip_ver'),
-    zzz;
+    arr_precip_ver = extractVal(data, 'arr_precip_ver');
 
-  // res.send(s);
-  res.json({searchstr:'http://meteoinfo.ru/forecasts5000/russia/'+searchstr,arr_temperature,arr_precip_val,arr_precip_ver});
+  try {
+
+    request.get(
+      'http://meteoinfo.ru/forecasts5000/russia/'+searchstr,
+      //'http://meteoinfo.ru/forecasts5000/russia/moscow-area/moscow',
+      //'http://meteoinfo.ru/forecasts5000/russia/nizhegorodskaya-area',
+      function(err, response, data) {
+        if(err || response.statusCode !== 200) {
+          res.json({ "error": err, statusCode: response.statusCode });
+        } else {
+          // data = JSON.parse(data);
+          res.json(data);
+        }
+      }
+    );
+
+    function extractVal(str,name) {
+      var re = new RegExp(name+'=(\\[\\{[^;]*);', 'i');
+      var s;
+      try {
+        s = eval(str.match(re)[1]);
+      } catch(err) {
+      }
+      return s;
+    }
+
+    // data = require('./1.js'),
+    arr_temperature = extractVal(data, 'arr_temperature');
+    arr_precip_val = extractVal(data, 'arr_precip_val');
+    arr_precip_ver = extractVal(data, 'arr_precip_ver');
+
+    // res.send(s);
+    res.json({searchstr:'http://meteoinfo.ru/forecasts5000/russia/'+searchstr,arr_temperature,arr_precip_val,arr_precip_ver});
+
+  } catch(err) {
+    res.json({err});
+  }
 
 });
 
